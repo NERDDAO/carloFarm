e787922139a979ca1de947d13d2c8032aeda3c56
mattim0use <mattimose@gmail.com>
2024-04-30 21:09:37 -0700

    stuff

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -40,20 +40,20 @@ export const Footer = () => {
               </>
             )}
           </div>
-          <SwitchTheme className={`pointer-events-auto ${isLocalNetwork ? "self-end md:self-auto" : ""}`} />
+          {/* <SwitchTheme className={`pointer-events-auto ${isLocalNetwork ? "self-end md:self-auto" : ""}`} /> */}
         </div>
       </div>
       <div className="w-full">
         <ul className="menu menu-horizontal w-full">
           <div className="flex justify-center items-center gap-2 text-sm w-full">
             <div className="text-center">
-              <a href="https://github.com/scaffold-eth/se-2" target="_blank" rel="noreferrer" className="link">
+              {/* <a href="https://github.com/scaffold-eth/se-2" target="_blank" rel="noreferrer" className="link">
                 Fork me
-              </a>
+              </a> */}
             </div>
-            <span>·</span>
+            {/* <span>·</span> */}
             <div className="flex justify-center items-center gap-2">
-              <p className="m-0 text-center">
+              {/* <p className="m-0 text-center">
                 Built with <HeartIcon className="inline-block h-4 w-4" /> at
               </p>
               <a
@@ -64,13 +64,13 @@ export const Footer = () => {
               >
                 <BuidlGuidlLogo className="w-3 h-5 pb-1" />
                 <span className="link">BuidlGuidl</span>
-              </a>
+              </a> */}
             </div>
-            <span>·</span>
+            {/* <span>·</span> */}
             <div className="text-center">
-              <a href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA" target="_blank" rel="noreferrer" className="link">
+              {/* <a href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA" target="_blank" rel="noreferrer" className="link">
                 Support
-              </a>
+              </a> */}
             </div>
           </div>
         </ul>
