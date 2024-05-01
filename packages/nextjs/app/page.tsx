ef487f0f7bf5537f420ab16814e33cf3375760bc
raigal-r <raquel.raigal@outlook.es>
2024-05-02 01:41:02 +0200

    annus pulse working correctly and responsive with the image

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -11,6 +11,12 @@ import { Address } from "~~/components/scaffold-eth";
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
 
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(true);
+  };
+
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10 ">
@@ -19,15 +25,49 @@ const Home: NextPage = () => {
           <Image src="/carlogo.png" alt="logo" className="flex justify-center items-center" width={300} height={300} />
         </h1>
         <div className="flex flex-row items-center">
-          <Image src="/sideEye.png" alt="logo" className="relative max-w-[50%] left-[50%] ml-[25%] mt-20" fill />
-
-          <Link
-            href="/farm"
-            onClick={() => {
-              console.log("clicked");
-            }}
-            className="absolute text-white p-2 w-[50px] h-[50px] rounded-lg cursor-pointer bottom-20 left-1/3 ml-52"
-          ></Link>
+          {/* <Image src="/sideEye.png" alt="logo" className="relative max-w-[50%] left-[50%] ml-[25%] mt-20" fill>
+            <Link
+              href="/farm"
+              onClick={() => {
+                console.log("clicked");
+              }}
+              className="absolute text-white p-2 rounded-lg cursor-pointer bottom-20 left-30 ml-52"
+            >
+              <Image
+                src="/carloAnus.png"
+                alt="Carlo anus"
+                width={50}
+                height={30}
+              />
+            </Link>
+          </Image> */}
+          <div className="relative">
+            <Image
+              src="/sideEye.png"
+              alt="logo"
+              width={600}
+              height={350}
+              className="relative mt-[-20%]"
+              layout="responsive" //I am using layout that it's a legacy thing, I have to change it, but like that it works
+            />
+            <Link
+              href="/farm"
+              onClick={() => {
+                console.log("clicked");
+              }}
+              className="absolute mt-[-26%] ml-[36.15%] width-[10px] z-10 block"
+            >
+              <Image
+                src="/carloAnus.png"
+                alt="Carlo anus"
+                width={10}
+                height={10}
+                className={`max-w-[20%] ${clicked ? "no-hover" : "pulse"}`}
+                onClick={handleClick}
+                layout="responsive" //I am using layout that it's a legacy thing, I have to change it, but like that it works
+              />
+            </Link>
+          </div>
         </div>
         FARM CARLO
       </div>
